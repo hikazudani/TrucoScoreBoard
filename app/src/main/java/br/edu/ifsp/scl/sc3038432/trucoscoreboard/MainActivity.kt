a38097a651af019989886a27bdf803d0e92c9585
hikazudani <lhjundi@proton.me>
2026-08-19 02:09:56 -0300

    feat: adds mao de onze

--- app/src/main/java/br/edu/ifsp/scl/sc3038432/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/trucoscoreboard/MainActivity.kt
@@ -2,6 +2,7 @@ package br.edu.ifsp.scl.sc3038432.trucoscoreboard
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.sc3038432.trucoscoreboard.databinding.ActivityMainBinding
 
@@ -10,7 +11,6 @@ class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-
     private var teamAPoints = 0
     private var teamBPoints = 0
 
@@ -20,19 +20,36 @@ class MainActivity : AppCompatActivity() {
 
         with(amb) {
 
+            val appButtons = listOf(
+                teamAPlusOneBt, teamBPlusOneBt, teamAPlusThreeBt, teamBPlusThreeBt
+            )
+
+            fun maoDeOnze() {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Mão de 11: ${if (teamAPoints == 11) "Team A" else "Team B"}",
+                    Toast.LENGTH_LONG
+                ).show()
+                teamAPlusThreeBt.visibility = View.INVISIBLE
+                teamBPlusThreeBt.visibility = View.INVISIBLE
+            }
+
             fun updateScoreDisplay() {
                 teamAScoreTv.text = teamAPoints.toString()
                 teamBScoreTv.text = teamBPoints.toString()
+
+                if (teamAPoints == 11 || teamBPoints == 11) {
+                    maoDeOnze()
+                }
             }
 
             listOf(
                 // "to" é uma função infixa que cria um Pair de dois valores
                 // cada par associa um botão numa lambda que será executada quando for clicado
-                teamAPlusOneBt to { teamAPoints ++ }, // = Pair(teamAPlusOneBt, { teamAPoints++ })
-                teamBPlusOneBt to { teamBPoints ++ },
-                teamAPlusThreeBt to {teamAPoints += 3},
-                teamBPlusThreeBt to {teamBPoints += 3}
-            ).forEach { (button, action) -> // action é a lambda associada
+                teamAPlusOneBt to { teamAPoints++ }, // = Pair(teamAPlusOneBt, { teamAPoints++ })
+                teamBPlusOneBt to { teamBPoints++ },
+                teamAPlusThreeBt to { teamAPoints += 3 },
+                teamBPlusThreeBt to { teamBPoints += 3 }).forEach { (button, action) -> // action é a lambda associada
                 button.setOnClickListener {
                     action() // executa a lambda
                     updateScoreDisplay()
@@ -43,9 +60,7 @@ class MainActivity : AppCompatActivity() {
                 teamAPoints = 0
                 teamBPoints = 0
                 updateScoreDisplay()
-                listOf(
-                    teamAPlusOneBt, teamBPlusOneBt, teamAPlusThreeBt, teamBPlusThreeBt
-                ).forEach { it.visibility = View.VISIBLE }
+                appButtons.forEach { it.visibility = View.VISIBLE }
             }
         }
     }
